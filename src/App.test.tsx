2df5017b9dd64a4c64bd04ac705408f82d18edfd
skp96 <patel.sunny293@gmail.com>
2022-04-12 16:17:30 -0400

    configured vite application to run tests

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test('displays hello world',async () => {
+    const { getByText } = render(<App />)
+    expect(getByText("Hello world!")).toBeInTheDocument();
+})
